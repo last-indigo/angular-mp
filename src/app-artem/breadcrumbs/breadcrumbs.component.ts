dedde57b8a167f6ee6dd5839a33378e4e4175c0b
Artem Drobotenko <artem_drobotenko@epam.com>
2017-03-13 19:49:09 +0200

    breadcrumbs: scaffolded component

---
+++ src/app-artem/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,12 @@
+/* breadcrumbs.component.ts */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'breadcrumbs',
+  templateUrl: './breadcrumbs.component.html',
+})
+export class BreadcrumbsComponent {
+  constructor() {
+    console.log('BreadcrumbsComponent constructor');
+  }
+}
