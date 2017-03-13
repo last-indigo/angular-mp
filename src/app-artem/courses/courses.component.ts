61a2b43729905463d721c7aa617d5fa51e5246c0
Artem Drobotenko <artem_drobotenko@epam.com>
2017-03-13 20:36:00 +0200

    implement ngOn... hooks https://angular.io/docs/ts/latest/guide/lifecycle-hooks.html

--- src/app-artem/courses/courses.component.ts
+++ src/app-artem/courses/courses.component.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  OnDestroy,
+
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+
+  SimpleChanges
+} from '@angular/core';
 import { CourseModel } from './course/course.model';
 import { CoursesService } from './courses.service';
 
@@ -7,19 +19,52 @@ import { CoursesService } from './courses.service';
   styleUrls: ['courses.component.scss'],
   templateUrl: 'courses.component.html'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit, OnChanges, OnDestroy,
+  AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit {
+
   public courses: CourseModel[];
 
   constructor(
     private coursesService: CoursesService
   ) {
-    coursesService.getCourses()
-      .then((res) => {
-        this.courses = res;
-      });
+    this.coursesService = coursesService;
   }
 
   public handleRemoveCourseParent($event) {
     console.log('handleRemoveCourseParent caught in CoursesComponent', $event);
   }
+
+  public ngOnInit(): void {
+    console.log('--- ngHooks in CoursesComponent: --- ngOnInit', arguments);
+
+    this.coursesService.getCourses()
+      .then((res) => {
+        this.courses = res;
+      });
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    console.log('--- ngHooks in CoursesComponent: --- ngOnChanges', arguments);
+  }
+
+  public ngOnDestroy(): void {
+    console.log('--- ngHooks in CoursesComponent: --- ngOnDestroy', arguments);
+  }
+
+  public ngAfterContentChecked(): void {
+    console.log('--- ngHooks in CoursesComponent: --- ngAfterContentChecked', arguments);
+  }
+
+  public ngAfterContentInit(): void {
+    console.log('--- ngHooks in CoursesComponent: --- ngAfterContentInit', arguments);
+  }
+
+  public ngAfterViewChecked(): void {
+    console.log('--- ngHooks in CoursesComponent: --- ngAfterViewChecked', arguments);
+  }
+
+  public ngAfterViewInit(): void {
+    console.log('--- ngHooks in CoursesComponent: --- ngAfterViewInit', arguments);
+  }
+
 }
