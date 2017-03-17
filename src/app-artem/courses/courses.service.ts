429ec46cf8d088a34560ee4edf483bbbd086960c
Artem Drobotenko <artem_drobotenko@epam.com>
2017-03-17 19:28:48 +0200

    courses.service: implement CRUD fixtures (createCourse, getCourseById, updateCourseById, removeCourseById)

--- src/app-artem/courses/courses.service.ts
+++ src/app-artem/courses/courses.service.ts
@@ -1,5 +1,6 @@
 import * as coursesMockData from './courses.api-mock.json';
 import { CourseModel } from './course/course.model';
+import * as _ from 'lodash';
 
 export class CoursesService {
   private coursesList: CourseModel[];
@@ -14,6 +15,57 @@ export class CoursesService {
     // imitate async
     return new Promise((resolve, reject) => {
       setTimeout(() => resolve(this.coursesList), 1000);
+    });
+  }
+
+  /*
+   * From task notes: "Do not use http module to solve a task."
+   */
+
+  public createCourse(rawModel: CourseModel): Promise<CourseModel> {
+    return new Promise((resolve, reject) => {
+      // $http.post()
+
+      setTimeout(() => {
+        console.warn('createCourse success');
+        this.coursesList = [rawModel, ...this.coursesList];
+        resolve(rawModel);
+      }, 400);
+    });
+  }
+
+  public updateCourseById(id: string, patch: any): Promise<CourseModel> {
+    return new Promise((resolve, reject) => {
+      // $http.patch()
+
+    });
+  }
+
+  public getCourseById(id: string): Promise<CourseModel> {
+    return new Promise((resolve, reject) => {
+      // $http.get()
+
+      setTimeout(() => {
+        const result = _.find(this.coursesList, {id});
+        if (result) {
+          console.warn('getCourseById returned item');
+          resolve(result);
+        } else {
+          reject('not found');
+        }
+      }, 400);
+    });
+  }
+
+  public removeCourseById(id: string): Promise<CourseModel[]> {
+    return new Promise((resolve, reject) => {
+      // $http.get()
+
+      setTimeout(() => {
+        console.warn('removeCourseById acknowledged delete');
+        this.coursesList = _.reject(this.coursesList, {id});
+        resolve(this.coursesList);
+      }, 400);
     });
   }
 }
