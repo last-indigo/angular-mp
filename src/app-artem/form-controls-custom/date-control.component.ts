a8606f90d770aaae74cfe1afef07f4df68409a2c
Artem Drobotenko <artem.drobotenko@gmail.com>
2017-05-10 01:56:25 +0300

    1.10. Forms. #1: draft for DateCustomControlComponent: implement setters and hooks (still not working OK - value doesnt appear in View)

--- src/app-artem/form-controls-custom/date-control.component.ts
+++ src/app-artem/form-controls-custom/date-control.component.ts
@@ -22,17 +22,41 @@ const CUSTOM_DATE_VALUE_ACCESSOR = {
     CUSTOM_DATE_VALUE_ACCESSOR
   ]
 })
+
+// @angular/forms/src/directives/control_value_accessor.d.ts
 export default class DateCustomControlComponent implements ControlValueAccessor {
-  public writeValue(obj: any): void {
+
+  public writeValue(newValue: any): void {
+    // change happened in the model, and we need to update view.
     console.log('writeValue');
+
+    this.valuePublic = newValue;
   }
 
   public registerOnChange(fn: any): void {
+    // is called when element is created
+    // we will call it when value in the component changed
     console.log('registerOnChange');
+    this.callMeWhenChangeHappens = fn;
   }
 
   public registerOnTouched(fn: any): void {
+    // registerOnChange. should
     console.log('registerOnTouched');
+    this.callMeWhenTouchedHappens = fn;
   }
+
+  private valuePrivate;
+  private get valuePublic() {
+    return this.valuePrivate;
+  };
+  private set valuePublic(newValue) {
+    this.valuePrivate = newValue;
+    this.callMeWhenChangeHappens(this.valuePrivate);
+  };
+
+  // these are overridden in registerOnChange/registerOnTouched
+  private callMeWhenChangeHappens: Function = () => {};
+  private callMeWhenTouchedHappens: Function = () => {};
 
 }
